04063b7601319c90b6250447540a5a571352b327
ibarbylev <ibarbylev@gmail.com>
2025-02-04 11:43:24 +0200

    some fixes

--- mini_project.py
+++ mini_project.py
@@ -33,19 +33,32 @@ while i < len(lines):
     line = line.replace("и т.д. и т.п.", UNIQ_MARK)
     # - строка не должна начинаться с пробелов, допустима только табуляция (отступ).
     line_lst = line.split("\t")
-    print(line_lst)     # ['   ', "   I'm fine. Thank you   for your question!           "]
     j = 0
-    new_line = ''
     while j < len(line_lst):
-
+        line_lst[j] = line_lst[j].strip()
         j += 1
+        line = "\t".join(line_lst)
 
     # - сделать так, чтобы все предложения начинались с большой буквы;
-    # - не было подряд несколько пробелов и знаков пунктуации;
-    # - Не забывайте, что точка не всегда значит конец предложения! Например, и т.д. и т.п.
+    # 1. Новая строка
+    # 2. ". " и alphabet
 
+
+    # - не было подряд несколько пробелов и знаков пунктуации;
+    new_line = ""
+    j = 0
+    while j < len(line) - 1: # делаем цикл по длине предложения, но -1 т.к. потом делаем замену +1 индекс
+        j += 1
+        if line[j] == line[j - 1] and (line[j] in PUNCTUATION_MARKS_AND_SPACE):
+            continue
+        new_line += line[j]
+
+    # - Не забывайте, что точка не всегда значит конец предложения! Например, и т.д. и т.п.
     line = line.replace(UNIQ_MARK, "и т.д. и т.п.")
     new_text += line + '\n'
     i += 1
 
+
 print(new_text)
+string = "   \t   I'm fine. Thank you   for your question!           "
+string = string.split('\t')
