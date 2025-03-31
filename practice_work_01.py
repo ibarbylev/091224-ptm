45fb7653a836b994b1fe1d1448189f722fc463d5
ibarbylev <ibarbylev@gmail.com>
2025-03-31 09:54:58 +0300

    some fixes

--- practice_work_01.py
+++ practice_work_01.py
@@ -1,36 +1,11 @@
-"""Пишем программу, которая обходит директорию и выводит информацию в формате,
-максимально похожем на ls в linux/macos (dir в windows).
-Делаем то же самое для директории и всех вложенных директорий.
-
-
-[DIR]      ./
-[FILE]          1042  practice_work_01_3_scandir.py
-[FILE]          1120  practice_work_01_2_listdir.py
-[FILE]          1150  practice_work_02.py
-[FILE]           825  practice_work_01.py
-[DIR]      homework/
-    [FILE]             1  homework_02.py
-    [FILE]             0  homework_01.py
-[DIR]      total_recall/
-    [FILE]             0  task_02.py
-    [FILE]             0  task_01.py
-    [FILE]             0  task_03.py
-[DIR]      theory/
-    [FILE]           633  theory_08_os_path.py
-    [FILE]           169  theory_10_pathlab.py
-
+"""Написать функцию more_than_value, которая проверяет, действительно ли
+все элементы списка больше заданной величины.
 """
 
-import os
+
+def more_than_value(nums: list[int | float], value: int | float) -> bool:
+    pass
 
 
-def list_directory(path):
-    root = ... # путь к текущей вложенной (рекурсивной) директории
-    # Вариант нахождения уровня вложенности папок
-    level = root.replace(path, '').count(os.sep)  # {os.sep} =  '/'- Posix; '\' - Windows
-    # Вариант нахождения размера файла
-    file_path = ...  # путь к файлу, размер которого измеряем
-    size = os.path.getsize(file_path)
-
-
-list_directory("..")
+lst = [2, 9, 4, 1, 12]
+print(more_than_value(lst, 1))
--- practice_work_02.py
+++ practice_work_02.py
@@ -1,30 +1,13 @@
-"""Напишите программу, которая принимает в качестве аргумента командной строки
- путь к директории и выводит список всех файлов с расширением .txt
- внутри этой директории и ее поддиректорий.
-Для этой задачи используйте рекурсивную функцию, которая будет обходить
-все поддиректории и искать файлы с расширением .txt.
-
-start_directory:  /home/su/PythonProjects/IT_career_hub/Python_Morning
-    [FILE]        2868  practice_tasks_07.txt
-    [FILE]        2980  practice_work_08.txt
-    [FILE]        3399  tasks.txt
-    [FILE]          38  persons.txt
-        [FILE]          27  output.txt
-        [FILE]          26  input.txt
-        [FILE]        1280  triangle_pascale.txt
-        [FILE]         238  article.txt
-    [FILE]          27  output.txt
-    [FILE]        1280  pascal_triangle.txt
-    [FILE]          26  input.txt
-    [FILE]        1280  triangle_pascale.txt
+"""Измените решение предыдущей задачи,
+применяя парадигму функционального программирования.
 """
-import os
 
 
-def get_all_txt_files(path, indent_level=0):
-    pass
+def more_than_value(nums: list[int | float], value: int | float) -> bool:
+    return ...
 
 
-start_directory = os.path.abspath('../..')
-print('start_directory: ', start_directory)
-get_all_txt_files(start_directory)
+lst = [2, 9, 4, 1, 12]
+print(more_than_value(lst, 1))
+print(more_than_value(lst, 0))
+
